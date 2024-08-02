e9ea552be06a9857377b68ccda30cc6be876cdb7
Udhbhav <21889udbhav@gmail.com>
2024-08-03 05:23:42 +0530

    Add update service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService: UsersService) {}
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        return body;
+        return this.usersService.create(body.email, body.password);
+    }
+
+    @Post('/update')
+    updateUser(@Body() body: Partial<User>) {
+        return this.usersService.update()
     }
 }
